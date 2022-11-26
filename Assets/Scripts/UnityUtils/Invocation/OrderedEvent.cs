7723c24ad0174f970f97e7553effbb0dc804c8fa
Artem Sukhliak <artyom.suhlyak@gmail.com>
2022-11-26 12:12:31 +0200

    Improve OrderedEvent debugger readability

--- Assets/Scripts/UnityUtils/Invocation/OrderedEvent.cs
+++ Assets/Scripts/UnityUtils/Invocation/OrderedEvent.cs
@@ -1,9 +1,12 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using JetBrains.Annotations;
 
 namespace UnityUtils.Invocation
 {
+    [DebuggerTypeProxy(typeof(DebuggerProxy))]
+    [DebuggerDisplay("SubscribersCount: {_orderedActions.Count}")]
     public class OrderedEvent
     {
         private readonly List<KeyValuePair<int, Action>> _orderedActions = new();
@@ -38,11 +41,39 @@ namespace UnityUtils.Invocation
         private class Comparator : IComparer<KeyValuePair<int, Action>>
         {
             public static readonly Comparator Default = new();
-                
+
             public int Compare(KeyValuePair<int, Action> left, KeyValuePair<int, Action> right)
             {
                 return left.Key.CompareTo(right.Key);
             }
         }
+
+#region DebuggerProxy
+        private class DebuggerProxy
+        {
+            private readonly OrderedEvent _orderedEvent;
+
+            [DebuggerBrowsable(DebuggerBrowsableState.RootHidden)]
+            public KeyValuePair<int, string>[] Values
+            {
+                get
+                {
+                    var result = new KeyValuePair<int, string>[_orderedEvent._orderedActions.Count];
+                    for (int i = 0; i < _orderedEvent._orderedActions.Count; i++)
+                    {
+                        var (order, action) = _orderedEvent._orderedActions[i];
+                        result[i] = new KeyValuePair<int, string>(order, $"'{action.Target}___{action.Method}'");
+                    }
+
+                    return result;
+                }
+            }
+
+            public DebuggerProxy(OrderedEvent orderedEvent)
+            {
+                _orderedEvent = orderedEvent;
+            }
+        }
+#endregion DebuggerProxy
     }
 }
