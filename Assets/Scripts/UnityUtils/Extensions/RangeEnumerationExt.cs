89aa940aef348ca489c6dfdd71977b76e3ec6f82
Artem Sukhliak <artyom.suhlyak@gmail.com>
2022-12-14 11:29:26 +0200

    WIP

--- Assets/Scripts/UnityUtils/Extensions/RangeEnumerationExt.cs
+++ Assets/Scripts/UnityUtils/Extensions/RangeEnumerationExt.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace UnityUtils.Extensions
 {
-    // Makes possible to enumerate with ranges. No heap allocations. Check EnumerationExamples for examples
+    // Makes possible to enumerate with ranges. Check EnumerationExamples for examples
     public static class RangeEnumerationExt
     {
         public static IntEnumerator GetEnumerator(this Range range)
@@ -12,12 +12,12 @@ namespace UnityUtils.Extensions
                 throw new NotSupportedException("Range must be closed");
             }
             
-            return new IntEnumerator(range);
+            return new IntEnumerator(range.Start.Value, range.End.Value);
         }
 
         public static IntEnumerator GetEnumerator(this int count)
         {
-            return new IntEnumerator(new Range(0, count));
+            return new IntEnumerator(0, count);
         }
 
         public ref struct IntEnumerator
@@ -25,11 +25,11 @@ namespace UnityUtils.Extensions
             private readonly int _end;
             private readonly int _step;
             
-            public IntEnumerator(Range range)
+            public IntEnumerator(int start, int end)
             {
-                _step = range.Start.Value <= range.End.Value ? 1 : -1;
-                Current = range.Start.Value - _step;
-                _end = range.End.Value;
+                _step = start <= end ? 1 : -1;
+                Current = start - _step;
+                _end = end;
             }
 
             public int Current { get; private set; }
