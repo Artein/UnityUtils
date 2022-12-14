f87cb1ffeb8d13fd418fabcacded5e4eb2063496
Artem Sukhliak <artyom.suhlyak@gmail.com>
2022-12-14 10:47:13 +0200

    Add ListExt.EnsureSize

--- Assets/Scripts/UnityUtils.Tests/Extensions/ListExtTests.cs
+++ Assets/Scripts/UnityUtils.Tests/Extensions/ListExtTests.cs
@@ -25,5 +25,25 @@ namespace Extensions
             
             Assert.That(list.Reverse_NoHeapAlloc, Is.Not.AllocatingGCMemory());
         }
+
+        [Test] public void EnsureSize_IncreasesSize()
+        {
+            const int initialCapacity = 10;
+            const int requiredSize = initialCapacity + 1;
+            var list = new List<int>(initialCapacity);
+
+            list.EnsureSize(requiredSize);
+
+            list.Count.Should().Be(requiredSize);
+        }
+
+        [Test] public void EnsureSize_FillsWithProvidedValue()
+        {
+            var list = new List<int> { 1, 2, 3 };
+
+            list.EnsureSize(6, 5);
+
+            list.Should().ContainInOrder(1, 2, 3, 5, 5, 5);
+        }
     }
 }
--- Assets/Scripts/UnityUtils/Extensions/ListExt.cs
+++ Assets/Scripts/UnityUtils/Extensions/ListExt.cs
@@ -1,11 +1,17 @@
+using System;
 using System.Collections.Generic;
 
 namespace UnityUtils.Extensions
 {
     public static class ListExt
     {
-        public static void Reverse_NoHeapAlloc<T>(this IList<T> list)
+        public static IList<T> Reverse_NoHeapAlloc<T>(this IList<T> list)
         {
+            if (list == null)
+            {
+                throw new ArgumentNullException(nameof(list));
+            }
+            
             int count = list.Count;
 
             for (int i = 0; i < count / 2; i++)
@@ -13,6 +19,39 @@ namespace UnityUtils.Extensions
                 var j = count - i - 1;
                 (list[i], list[j]) = (list[j], list[i]);
             }
+
+            return list;
+        }
+
+        public static List<T> EnsureSize<T>(this List<T> list, int size, T value = default)
+        {
+            if (list == null)
+            {
+                throw new ArgumentNullException(nameof(list));
+            }
+
+            if (size < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(size));
+            }
+
+            int count = list.Count;
+            if (count < size)
+            {
+                int capacity = list.Capacity;
+                if (capacity < size)
+                {
+                    list.Capacity = Math.Max(size, capacity * 2);
+                }
+
+                while (count < size)
+                {
+                    list.Add(value);
+                    ++count;
+                }
+            }
+
+            return list;
         }
     }
 }
