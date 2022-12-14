e42cae3828ce017286c8cdfd22783f125c0ccf67
Artem Sukhliak <artyom.suhlyak@gmail.com>
2022-12-14 12:03:49 +0200

    Add tests to improve code coverage

--- Assets/Scripts/UnityUtils.Tests/Extensions/ListExtTests.cs
+++ Assets/Scripts/UnityUtils.Tests/Extensions/ListExtTests.cs
@@ -36,7 +36,7 @@ namespace Extensions
             action.Should().Throw<ArgumentNullException>();
         }
 
-        [Test] public void EnsureSize_IncreasesSize()
+        [Test] public void EnsureSize_Method_IncreasesSize()
         {
             const int initialCapacity = 10;
             const int requiredSize = initialCapacity + 1;
@@ -47,13 +47,31 @@ namespace Extensions
             list.Count.Should().Be(requiredSize);
         }
 
-        [Test] public void EnsureSize_FillsWithProvidedValue()
+        [Test] public void EnsureSize_Method_FillsWithProvidedValue()
         {
             var list = new List<int> { 1, 2, 3 };
 
             list.EnsureSize(6, 5);
 
             list.Should().ContainInOrder(1, 2, 3, 5, 5, 5);
+        }
+        
+        [Test] public void EnsureSize_Method_OnNullList_Throws_ArgumentNullException()
+        {
+            List<int> list = null;
+
+            Action action = () => list.EnsureSize(1);
+
+            action.Should().Throw<ArgumentNullException>();
+        }
+        
+        [Test] public void EnsureSize_Method_WithNegativeSize_Throws_ArgumentOutOfRangeException()
+        {
+            var list = new List<int>();
+
+            Action action = () => list.EnsureSize(-100);
+
+            action.Should().Throw<ArgumentOutOfRangeException>();
         }
     }
 }
